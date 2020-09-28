9c715d9000257d8283e38b4774e46592044eca55
jonas-agapito <71768050+jonas-agapito@users.noreply.github.com>
2020-09-28 16:20:36 -0300

    Create geojson.js

---
+++ geojson.js
@@ -0,0 +1,18 @@
+var iluminacaoInterna = {
+  "type": "Feature",
+  "features": [
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -35.911886206839725,
+          -7.216404930930547
+        ]
+      },
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#fff100"
+      }
+    }
+  ]
+}
