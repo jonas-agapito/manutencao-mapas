18d48b952acd43a1812fadb2d6483826802cf9c2
jonas-agapito <71768050+jonas-agapito@users.noreply.github.com>
2020-09-28 18:06:42 -0300

    Sem o primeiro ponto

--- geojson.js
+++ geojson.js
@@ -3,7 +3,6 @@ var iluminacaoInterna = {
 "name": "iluminacaoInterna",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "marker-image": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/HERE_logo.svg/180px-HERE_logo.svg.png" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -35.911886206839725, -7.216404930930547 ] ] } },
 { "type": "Feature", "properties": { "marker-color": "#fff100" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -35.911335390755823, -7.216581206040059 ] ] } },
 { "type": "Feature", "properties": { "marker-color": "#fff100" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -35.911305753931572, -7.216701742415245 ] ] } },
 { "type": "Feature", "properties": { "marker-color": "#fff100" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -35.911753840594642, -7.21645589992975 ] ] } },
